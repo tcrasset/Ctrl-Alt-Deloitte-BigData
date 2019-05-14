8a715806b3feb8584d7e5aea3a9969d641d1487e
tcrasset <tomcrasset@gmail.com>
2019-05-14 12:25:33 +0200

    Remove rows with more than half of NANs

--- preprocessing.py
+++ preprocessing.py
@@ -5,5 +5,13 @@ import csv
 df = pd.read_csv('Devices.csv', delimiter=';', header=None, error_bad_lines=False, encoding='latin1')
 
 
-df.dropna(thresh=5,inplace=True)
-df.to_csv("Devices_clean.csv")
+
+print(len(df))
+print(df.isna().sum())
+
+clean_df = df[[c for c in df if df[c].isnull().sum() < len(df)/2]]
+
+print(clean_df.isna().sum())
+
+clean_df.to_csv("Devices_clean.csv")
+
