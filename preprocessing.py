2a18068c491659ade7f4dc7392417192ba81cc05
tcrasset <tomcrasset@gmail.com>
2019-05-14 12:36:52 +0200

    Remove row and column index

--- preprocessing.py
+++ preprocessing.py
@@ -2,16 +2,15 @@ import pandas as pd
 import csv
 
 
-df = pd.read_csv('Devices.csv', delimiter=';', header=None, error_bad_lines=False, encoding='latin1')
-
-
-
-print(len(df))
-print(df.isna().sum())
+df = pd.read_csv('Devices.csv', delimiter=';', error_bad_lines=False, encoding='latin1')
+print("Length of dirty Devices", len(df))
+# print(df.isna().sum())
 
 clean_df = df[[c for c in df if df[c].isnull().sum() < len(df)/2]]
 
-print(clean_df.isna().sum())
+print([c for c in df.columns if c not in clean_df.columns])
+print("Length of cleaned Devices", len(clean_df))
+# print(clean_df.isna().sum())
 
-clean_df.to_csv("Devices_clean.csv")
+clean_df.to_csv("Devices_clean.csv", index=False)
 
